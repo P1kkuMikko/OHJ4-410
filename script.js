d08686dac2fc7b9c44edd44a104b5fc62f91346e
Mikko Nykänen <143422957+P1kkuMikko@users.noreply.github.com>
2024-06-13 11:10:33 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function kotiin() {
     document.getElementById("kaverilista2").innerHTML = "";
 }
 
-function showVersion1() { 
+function showVersion1() {
     document.getElementById("home").style.display = "none";
     document.getElementById("version1").style.display = "block";
     document.getElementById("kaverilista").style.display = "block";
@@ -20,40 +20,40 @@ function showVersion2() {
 
 function Aloita() {
     var kaverit = [];
-    var promptMessage = "Anna kaverin nimi";
+    var promt = "Anna kaverin nimi";
+    var nimi;
 
     while (kaverit.length < 10) {
-        var nimi = prompt(promptMessage);
-        if (nimi === null || nimi === "") {
-            promptMessage = "Nimi ei voi olla tyhjä. Anna kaverin nimi uudelleen:";
-            continue;
+        nimi = prompt(promt);
+        if (nimi === null) {
+            break;
+        }
+        while (nimi == "") {
+            nimi = prompt("Nimi ei voi olla tyhjä. Anna kaverin nimi uudelleen:");
+            if (nimi === null) {
+                break;
+            }
+        }
+        if (nimi === null) {
+            break;
         }
         kaverit.push(nimi);
-        promptMessage = "Anna seuraava kaverin nimi";
+        promt = "Anna seuraava kaverin nimi";
     }
 
     var kaverilista = document.getElementById("kaverilista");
-    kaverilista.innerHTML = "<ol>" + kaverit.map(function(nimi) {
-        return "<li>" + nimi + "</li>";
-    }).join('') + "</ol>";
-}
-
-function lisaaKaveri() {
-    var input = document.getElementById('kaveri2');
-    var list = document.getElementById('kaverilista2');
-
-    var li = document.createElement('li');
-    li.textContent = input.value;
-    list.appendChild(li);
-
-    input.value = '';
+    kaverilista.innerHTML = "<ol>";
+    for (var i = 0; i < kaverit.length; i++) {
+        kaverilista.innerHTML += "<li>" + kaverit[i] + "</li>";
+    }
+    kaverilista.innerHTML += "</ol>";
 }
 
 function jarjestaKaverit() {
     var list = document.getElementById('kaverilista2');
     var items = Array.from(list.getElementsByTagName('li'));
 
-    items.sort(function(a, b) {
+    items.sort(function (a, b) {
         return a.textContent.localeCompare(b.textContent);
     });
 
@@ -61,9 +61,19 @@ function jarjestaKaverit() {
         list.removeChild(list.firstChild);
     }
 
-    items.forEach(function(item) {
+    items.forEach(function (item) {
         list.appendChild(item);
     });
+}
+
+function lisaaKaveri2() {
+    var input = document.getElementById('kaveri2');
+    var list = document.getElementById('kaverilista2');
+    var li = document.createElement('li');
+    li.textContent = input.value;
+    list.appendChild(li);
+    input.value = '';
+
 }
 
 function poistaKaveri() {
